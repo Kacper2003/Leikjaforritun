854efe075558046d76ac03b5bfa60e24a3bf2487
Kacper2003 <54663437+Kacper2003@users.noreply.github.com>
2021-09-28 16:41:05 +0000

    Add files via upload

---
+++ Assets/Scripts/Daudi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Daudi : MonoBehaviour
+{
+    public Transform leikmadur;
+    
+    void Update()
+    {
+        //Debug.Log("halló");
+        if (transform.position.y<=0)
+        {
+           SceneManager.LoadScene("Menu");//byrja á byrjun ef leikmaður fellur fram af brún eða í vatnið
+        }
+    }
+}
